219ee41092f928ad7bc926084b4e2395cfb5621e
deostroll <deostroll@gmail.com>
2015-11-13 13:57:35 +0530

    axis helper

--- src/main.js
+++ src/main.js
@@ -10,19 +10,11 @@ function onPageLoad() {
 
   document.body.appendChild(renderer.domElement);
   var controls = new THREE.OrbitControls(camera, renderer.domElement);
-  camera.position.set(0, 0, 15);
+  camera.position.set(-25, 25, 25);
   camera.lookAt(scene.position);
 
-  var
-    dir = new THREE.Vector3(1,0,0),
-    origin = new THREE.Vector3(0,0,0);
-
-  var xaxis = new THREE.ArrowHelper(dir, origin, 10, 0xffff00);
-  var yaxis = new THREE.ArrowHelper(new THREE.Vector3(0,1,0), origin, 10, 0xff0000);
-  var zaxis = new THREE.ArrowHelper(new THREE.Vector3(0,0,1), origin, 10, 0x0000ff);
-
-  scene.add(xaxis);
-  scene.add(yaxis, zaxis);
+  var axes = new THREE.AxisHelper(10);
+  scene.add(axes);
 
   //renderer.render(scene, camera);
   function animate() {
