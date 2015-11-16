7cdc8a0ba96be2e73ae9645ee8a72260bc8111e1
deostroll <deostroll@gmail.com>
2015-11-16 23:50:49 +0530

    serpenski generated successfully with holes

--- src/main.js
+++ src/main.js
@@ -10,9 +10,12 @@ function onPageLoad() {
 
   document.body.appendChild(renderer.domElement);
   var controls = new THREE.OrbitControls(camera, renderer.domElement);
-  camera.position.set(-25, 25, 25);
+  var l = 8;
+  // camera.position.set(4 + l/2, 2 + l/6 * Math.sqrt(3) ,0);
+  // camera.position.set(8, 2+ 8/ 6 * Math.sqrt(3), -15)
   camera.lookAt(scene.position);
-
+  // camera.lookAt(8, 2+ 8/ 6 * Math.sqrt(3), 0)
+  camera.position.set(-15, 15, 15)
   var axes = new THREE.AxisHelper(10);
   scene.add(axes);
 
@@ -30,16 +33,10 @@ function onPageLoad() {
         by = pts[3],
         cx = pts[4],
         cy = pts[5];
-
-      // var midPoints = [
-      //     (ax + bx)/2, (ay + by)/2 ,
-      //     (bx + cx)/2, (by + cy)/2 ,
-      //     (cx + ax)/2, (cy + ay)/2
-      // ];
       var
-        m1x = (ax + bx)/2, m1y = (ay + by)/2,
-        m2x = (bx + cx)/2, m2y = (by + cy)/2,
-        m3x = (cx + ax)/2, m3y = (cy + ay)/2
+        m1x = (ax + bx)/2, m1y = (ay + by)/2, //side ab
+        m2x = (bx + cx)/2, m2y = (by + cy)/2, //side bc
+        m3x = (cx + ax)/2, m3y = (cy + ay)/2  //side ca
       ;
 
       var t1 = [
@@ -85,13 +82,22 @@ function onPageLoad() {
   }
 
   var points = [
-    4,2 ,
-    8,2 ,
-    6, 2 + 2 * Math.sqrt(3)
+    4     , 2 ,
+    4+l   , 2 ,
+    4+l/2 , 2 + l/2 * Math.sqrt(3)
   ];
-
+  function _print(pt) {
+    console.log(JSON.stringify(pt));
+  }
   var result = serpenski(points, 2);
   var mesh = new THREE.Mesh();
+  var getVector2Array = function(holePoints) {
+    var VArray = [];
+    for(var k = 0, l = holePoints.length; k < l; k+=2) {
+      VArray.push(new THREE.Vector2(holePoints[k], holePoints[k+1]));
+    }
+    return VArray;
+  };
   var getHolePoints = function(pts, d) {
     var p = pts[0], q = pts[1];
     var l = Math.abs(p - pts[2]);
@@ -106,43 +112,43 @@ function onPageLoad() {
 
   var mat1 = new THREE.MeshBasicMaterial({ color: THREE.ColorKeywords['cyan']});
   var mat2 = new THREE.MeshBasicMaterial({ color: THREE.ColorKeywords['grey']});
-  var getVector2Array = function(holePoints) {
-    var VArray = [];
-    for(var k = 0, l = holePoints.length; k < l; k+=2) {
-      VArray.push(new THREE.Vector2(holePoints[i], holePoints[i+1]));
-    }
-    return VArray;
-  };
+
   var extrusionSettings = {
-    amount: 1,
+    amount: 3,
     bevelEnabled: false
   };
-
+  // debugger;
   for (var i = 0, triangles = result.triangles, j = triangles.length; i < j; i++) {
       var pt = triangles[i];
+      //defining shape
       var shape = new THREE.Shape();
       shape.moveTo(pt[0], pt[1]);
       shape.lineTo(pt[2], pt[3]);
       shape.lineTo(pt[4], pt[5]);
       shape.lineTo(pt[0], pt[1]);
+
+      //defining hole for shape
       var holePoints = getHolePoints(pt, 0.05);
       var VArray = getVector2Array(holePoints);
       var holePath = new THREE.Path(VArray);
       shape.holes.push(holePath);
+
       var geo = shape.extrude(extrusionSettings);
       var mesh = new THREE.Mesh(geo, mat1);
 
+      //defining inner shape
       var inner = new THREE.Shape(VArray);
-      var innerHolePoints = getHolePoints(holePoints)
+      var innerHolePoints = getHolePoints(holePoints, 0.05)
       var innerVArray = getVector2Array(innerHolePoints);
       var innerHolePath = new THREE.Path(innerVArray);
       inner.holes.push(innerHolePath);
 
-      var innerGeo = shape.extrude(extrusionSettings);
+      var innerGeo = inner.extrude(extrusionSettings);
       var innerMesh = new THREE.Mesh(innerGeo, mat2);
-      innerMesh.translateX(5);
-      mesh.add(innerMesh);
+      // innerMesh.translateX(5);
+      //mesh.add(innerMesh);
       scene.add(mesh);
+      scene.add(innerMesh);
   }
 
   //renderer.render(scene, camera);
